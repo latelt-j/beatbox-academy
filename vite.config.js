7d1968cca9690956f06ec9a012a60355cd016078
Sylvain Blanc <sylvain.blanc@dawex.com>
2024-06-27 22:40:51 +0200

    base not baseurl

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  baseUrl: "beatbox-academy",
+  base: "/beatbox-academy/",
   plugins: [
     vue(),
     vueDevTools(),
